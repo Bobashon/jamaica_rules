47f6db7cc08fbc97674cbf3ae0fe01b211995d32
Macario De La Peña <mack.delapena@gmail.com>
2013-05-25 21:08:41 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JamaicaRules::Application.config.secret_token = 'd1870e51a8841325cc3ca4d5c94fdb62b857010ae04e8372b08642c1b6bcd5c75fb659ebf84af8cf33403972739d6b106533a3f8501c22df5fafed979fe830e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+JamaicaRules::Application.config.session_store :cookie_store, key: '_jamaica_rules_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# JamaicaRules::Application.config.session_store :active_record_store
